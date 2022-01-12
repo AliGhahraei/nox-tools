ad85553fc225bfee969295f904780284a2e429f3
Ali Ghahraei <aligf94@gmail.com>
2022-01-11 23:01:47 -0600

    Add nox to dependencies and variables to __all__

--- nox_tools/__init__.py
+++ nox_tools/__init__.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+from nox_tools.sessions import (
+    config, install_current_package, install_requirements,
+    install_test_packages, linting, tests, typing,
+)
+
+__all__ = [
+    'config', 'install_current_package', 'install_requirements',
+    'install_test_packages', 'linting', 'tests', 'typing',
+]
